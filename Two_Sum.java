664c00507d76f7b4d845b92a48b0a5cdae749267
Jiali Wang <wjlbmw@gmail.com>
2018-10-06 16:15:57 -0700

    easy:two sums

---
+++ Two_Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i<nums.length; i++ ) {
+            indexMap.put(nums[i], i);
+        }
+        
+        for (int i = 0; i<nums.length; i++) {
+            int remainValue = target - nums[i];
+            if (indexMap.containsKey(remainValue) && indexMap.get(remainValue)!= i) {
+                return new int[]{indexMap.get(remainValue), i};
+            }
+        }
+        throw new IllegalArgumentException("no available solution");
+    }
+}
