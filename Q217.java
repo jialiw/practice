fcf5c57d2837df4c04903f19c51ad9482d844afd
Jiali Wang <wjlbmw@gmail.com>
2018-10-22 16:53:19 -0700

    Q217.Contains Duplicate. remove one solution

--- Q217.java
+++ Q217.java
@@ -42,18 +42,3 @@ class Solution {
 	}
 }
 
-// naive O(n^2) but 1ms solution 
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        if (nums == null || nums.length < 2 ) {
-            return false; 
-        }
-        for (int i=1; i<nums.length; i++) {
-            for(int j=i-1; j>=0; j--){
-                if(nums[i] > nums[j]) break; 
-                if (nums[i] == nums[j]) return true;
-            }
-        }
-        return false;
-    }
-}
